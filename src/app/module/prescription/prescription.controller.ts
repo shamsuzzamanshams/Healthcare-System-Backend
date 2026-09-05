89e9e98040370da14e80a700cb7229d67605874c
shamsuzzamanshams <shams30200@gmail.com>
2026-09-05 14:37:58 +0600

    feat: creating api for create prescription

---
+++ src/app/module/prescription/prescription.controller.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from "express";
+import httpStatus from "http-status";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { PrescriptionService } from "./prescription.service";
+
+
+const createPrescription = catchAsync(async (req: Request, res: Response) => {
+    const payload = req.body;
+    const user = req.user!;
+
+    const result = await PrescriptionService.createPrescription(payload, user);
+    sendResponse(res, {
+        statusCode: httpStatus.CREATED,
+        success: true,
+        message: "Prescription Created And Emailed To Patient Successfully",
+        data: result,
+    });
+});
+
+const getSinglePrescription = catchAsync(async (req: Request, res: Response) => {
+    const appointmentId = req.params.appointmentId as string;
+    const user = req.user!;
+
+    const result = await PrescriptionService.getSinglePrescription(
+        appointmentId,
+        user,
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Prescription Retrieved Successfully",
+        data: result,
+    });
+});
+
+export const PrescriptionController = {
+    createPrescription,
+    getSinglePrescription,
+};
